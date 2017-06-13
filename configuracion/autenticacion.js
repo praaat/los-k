ec3dc980126fa4fc3e8198b19a0b36442750c67e
panchito1234 <frrebolledo@alumnos.uai.cl>
2017-06-13 10:17:20 -0400

    Add files via upload

---
+++ configuracion/autenticacion.js
@@ -0,0 +1,31 @@
+// config/auth.js
+
+// expose our config directly to our application using module.exports
+module.exports = {
+
+    'facebookAuth' : {
+        'clientID'      : 'your-secret-clientID-here', // your App ID
+        'clientSecret'  : 'your-client-secret-here', // your App Secret
+        'callbackURL'   : 'http://localhost:8080/autenticacion/facebook/callback'
+    }
+};
+
+<script>
+window.fbAsyncInit = function() {
+    FB.init({
+        appId      : '187361745126452',
+        cookie     : true,
+        xfbml      : true,
+        version    : 'v2.8'
+    });
+    FB.AppEvents.logPageView();
+};
+
+(function(d, s, id){
+    var js, fjs = d.getElementsByTagName(s)[0];
+    if (d.getElementById(id)) {return;}
+    js = d.createElement(s); js.id = id;
+    js.src = "//connect.facebook.net/en_US/sdk.js";
+    fjs.parentNode.insertBefore(js, fjs);
+}(document, 'script', 'facebook-jssdk'));
+</script>
